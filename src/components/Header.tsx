663b326f481f9bdf23f13c0680c6fb991cbc53c3
Kelvin <kierulfkelvin@gmail.com>
2025-06-11 18:49:31 +0800

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+// src/components/Header.tsx
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/projects">Projects</a>
+        <a href="/contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
