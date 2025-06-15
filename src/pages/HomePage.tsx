d99f0c751c521b1113c778b679cac494d882f093
Kelvin <kierulfkelvin@gmail.com>
2025-06-15 08:33:51 +0800

    Home Page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,33 @@
 function HomePage() {
   return (
     <div>
-      <h1>About Me</h1>
+      <h1>Welcome to My Portfolio</h1>
       <p>
-        This is where I'll write a bit about myself, my skills, and my
-        experience.
+        Hi! I am Kelvin Pehrson P. Kierulf. Currently a 3rd Year Computer
+        Science Student at Cebu Institute of Technology.
+        <br></br>
+        My coursework has provided me with a solid foundation in programming
+        languages such as C++, Java, and Kotlin, as well as database management
+        with MySQL.
+        <br></br>
+        Beyond my academic achievements, I have actively sought opportunities to
+        apply my skills to real-world projects. Notably, I led the UI/UX
+        implementation, API integration, and quality assurance for "Asa Ta Bai,"
+        a Jeepney Information Mobile App. Additionally, I independently
+        designed, developed, and deployed "Camera Connect," a mobile and desktop
+        application that enables mobile phone cameras to be used as desktop
+        webcams. These experiences have honed my ability to work both
+        independently and collaboratively, manage project timelines, and deliver
+        effective solutions.
+        <br></br>
+        My technical skills include C++, Java, Flutter, PHP, C, MySQL, Dart, and
+        Kotlin. I am also proficient in essential soft skills such as
+        communication, teamwork, team building, and documentation.
       </p>
+
+      <h3>My projects</h3>
+      <ol>Nutrilens</ol>
+      <ol> Gemini Integration</ol>
     </div>
   );
 }
